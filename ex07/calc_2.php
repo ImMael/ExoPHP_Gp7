dca5f484e3d765ca71c6b47375dfa6576cbc31ff
Mael <72377688+ImMael@users.noreply.github.com>
2021-02-09 12:29:40 +0100

    Update calc_2.php

--- ex07/calc_2.php
+++ ex07/calc_2.php
@@ -1,7 +1,18 @@
 <?php
 
 function calc2($string){
-$arr = preg_split('//', $string, -1, PREG_SPLIT_NO_EMPTY);
+    if (preg_match('/[\s,]+/', $string)) {
+        $arr = preg_split("/[\s,]+/", $string);
+    }else {
+        $arr = preg_split('//', $string, -1, PREG_SPLIT_NO_EMPTY);
+    }
+
+// foreach ($arr as $key => $value) {
+//     if ($arr[$key] == " ") {
+
+//         array_splice($arr, $key, $key);
+//     }
+// }
 print_r($arr);
 
 if (is_numeric($arr[0]) == false || is_numeric($arr[2])==false ) {
